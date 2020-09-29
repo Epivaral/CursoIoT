952426d402405be60ee25a9c077c7e503616f06e
Eduardo Pivaral <39424641+Epivaral@users.noreply.github.com>
2020-09-28 21:16:38 -0600

    Add files via upload

---
+++ Modulo 2/03_Toggle_and_delay_Push_Button_y_LED/03_Toggle_and_delay_Push_Button_y_LED.ino
@@ -0,0 +1,31 @@
+const int PINout = 4; //El GPIO pin que usaremos para el led
+const int PINin = 5; //EL GPIO pin que usaremos para el push button
+
+void setup() {
+  pinMode(PINin, INPUT_PULLUP); //Establecemos el GPIO pin 5 como entrada
+  pinMode(PINout, OUTPUT); //Establecemos el GPIO pin 4 como salida
+}
+
+// La funcion loop corre infinitamente
+bool Estado = 0;
+bool EstadoInterno = 1;
+void loop() {
+  
+  // Leemos constamentente el estatus del push button, 
+  // ya que usamos una configuracion PULL UP, el 0 es cuando presionamos el boton y 1 es cuando lo dejamos de presionar
+  Estado = digitalRead(PINin);
+  if (Estado == LOW)
+    {
+      EstadoInterno = !EstadoInterno;
+      delay (200); // un retardo de 200 milisegundos luego de cambiar el estado para evitar el efecto rebote
+    }
+    
+  if(EstadoInterno==0)
+    {
+      digitalWrite(PINout, HIGH); //Mandamos un 1 logico a la salida
+    }
+  else
+    {
+      digitalWrite(PINout, LOW); //Mandamos un 0 logico a la salida
+    }               
+}
