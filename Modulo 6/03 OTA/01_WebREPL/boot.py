31410ac4adbb47c70ec266fc69e74eff7885d531
Eduardo Pivaral <epivaral@galileo.edu>
2021-10-23 19:00:12 -0600

    video 3 y 4

---
+++ Modulo 6/03 OTA/01_WebREPL/boot.py
@@ -0,0 +1,36 @@
+#imports
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+#-------------------------------
+
+import network
+from time import time
+
+sta_if = network.WLAN(network.STA_IF) #definimos como Station nuestro board
+
+if sta_if.active() == False: # validamos si STATION esta activado o no
+  sta_if.active(True)
+  
+sta_if.connect('<SSID>', '<PWD>') #Cambiar por tu SSID y Password
+
+print('Conectando...')
+
+t = time()
+
+while sta_if.isconnected() == False:
+    if(time()-t)>20: #timeout de n segundos para evitar bloquear el board  
+        break
+
+   
+if sta_if.active():
+  print('Station Activado')
+  print('IP Board:', sta_if.ifconfig())
+
+import webrepl
+webrepl.start()
+
+
